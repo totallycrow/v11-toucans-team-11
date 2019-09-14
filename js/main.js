5a9a9061d0e015516051eeb1b8073b40242a2ea8
Calogero Napoli <calogero.napoli92@gmail.com>
2019-09-14 17:22:16 +0200

    corrected two bugs with js and css when risizing the window in tablet and mobile would make disappear the hamburger menu and also the top navbar, corrected also another bug about refreshing the js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,41 @@
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-if ( $(window).width() < 1281) {
-  document.getElementById("myFunction").addEventListener("click", myFunction);
 function myFunction() {
   var x = document.getElementsByClassName('myLinks');
   const divyArray = Array.from(x);
-  let newArray = divyArray.map((currentValue, index, divyArray) => {
-    if (currentValue.style.display === 'block')
-    {currentValue.style.display = 'none';}
-    else {currentValue.style.display = 'block';}
-  });}}
+  divyArray.forEach(currentValue => {
+    currentValue.classList.toggle('show');
+  });
+}
 
-  var slideIndex = 0;
-  showSlides();
+var slideIndex = 0;
+showSlides();
 
-  function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 5000); // Change image every 5 seconds
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName('mySlides');
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+window.addEventListener('load', () => {
+  if ($(window).width() < 1281) {
+    document.getElementById('myFunction').addEventListener('click', myFunction);
+  }
+});
+
+window.addEventListener('resize', () => {
+  if ($(window).width() < 1281) {
+    document
+      .getElementById('myFunction')
+      .removeEventListener('click', myFunction);
+    document.getElementById('myFunction').addEventListener('click', myFunction);
+  }
+});
