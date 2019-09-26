22796c023dcc3278566d069505db5cec9d63be43
Adrian Gajda <adrian.gajda@hotmail.com>
2019-09-26 18:08:29 +0100

    Add base megamenu

--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
-
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-if ( $(window).width() < 1281) {
-function myFunction() {
-  var x = document.getElementsByClassName('myLinks');
-  const divyArray = Array.from(x);
-  let newArray = divyArray.map((currentValue, index, divyArray) => {
-    if (currentValue.style.display === 'block')
-    {currentValue.style.display = 'none';}
-    else {currentValue.style.display = 'block';}
-  });}}
-
-  var slideIndex = 0;
-  showSlides();
-
-  function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 5000); // Change image every 5 seconds
+if ($(window).width() < 1281) {
+  function myFunction() {
+    var x = document.getElementsByClassName("myLinks");
+    const divyArray = Array.from(x);
+    let newArray = divyArray.map((currentValue, index, divyArray) => {
+      if (currentValue.style.display === "block") {
+        currentValue.style.display = "none";
+      } else {
+        currentValue.style.display = "block";
+      }
+    });
   }
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
